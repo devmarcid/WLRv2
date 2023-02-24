1221b76ee3dcff297da20ed8057a5bff7eb676f9
marcid <marcid@pop-os.localdomain>
2023-02-24 12:32:06 -0600

    Fixed command duplicates

--- deploy-commands.js
+++ deploy-commands.js
@@ -15,6 +15,16 @@ for (const file of commandFiles) {
 // Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(token);
 
+
+
+// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+// 	.then(() => console.log('Successfully deleted all guild commands.'))
+// 	.catch(console.error);
+
+// // for global commands
+// rest.put(Routes.applicationCommands(clientId), { body: [] })
+// 	.then(() => console.log('Successfully deleted all application commands.'))
+// 	.catch(console.error);
 // and deploy your commands!
 (async () => {
 	try {
