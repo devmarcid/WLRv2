906a5b43e8a655d36ed9e06d1da354cf73ced221
marcid <marcid@pop-os.localdomain>
2023-03-02 15:07:13 -0600

    REDID SKULLBOARD

--- skullboard/skullboard.js
+++ skullboard/skullboard.js
@@ -1,11 +1,48 @@
-const skullboard = require("./skullboard_guilds/data/skullboard.json");
-const homiecordSkullboard = require("./skullboard_guilds/homiecord.js");
+const skullboard = require("./data/skullboard.json");
 const skull_emote = "💀";
+const fs = require("fs");
+const { EmbedBuilder } = require("discord.js");
 
-function initSkullboard(client, reaction, user) {
+function getEmbed(skullCount, reaction) {
+    const e = new EmbedBuilder();
+    var author = {}
+    var linkField = {}
+    author['name'] = reaction.message.author.username;
+    author['iconURL'] = reaction.message.author.avatarURL();
+    linkField['name'] = 'Source';
+    linkField['value'] = reaction.message.url;
+    e.setAuthor(author)
+    .addFields(linkField)
+    .setColor(0xA020F0);
+    return e;
+}
 
-    if (reaction.message.guild.id == '844718328928796694') {
-        homiecordSkullboard.startSkullboard(client, reaction, user, skull_emote);
+async function initSkullboard(client, reaction, user) {
+
+
+    var skullCount = reaction.message.reactions.cache.get(skull_emote).count;
+    if (reaction.emoji.name !== skull_emote) return;
+    
+    const skullChannel = reaction.message.guild.channels.cache.find(channel => channel.name.toLowerCase() === `skullboard-${skull_emote}`);
+
+    if (!skullChannel) return console.log("Skullchannel not found.");
+
+    if (((reaction.message.id.toString() in skullboard) == false) && skullCount > 1) {
+        var newSkull = skullboard;
+        //make message for skullpost
+        var skullMessage = `${skull_emote} **${skullCount}**`;
+        var skullPost = await skullChannel.send({ content: skullMessage, embeds: [getEmbed(skullCount, reaction)] });
+        newSkull[reaction.message.id.toString()] = skullPost.id.toString();
+        fs.writeFileSync('./skullboard/data/skullboard.json', JSON.stringify(newSkull));
+        return;
+    } else {
+        if (skullCount <= 1) return;
+        var skullPost =  await skullChannel.messages.fetch(skullboard[reaction.message.id.toString()]);
+        if (skullPost) {
+            skullPost.edit({content: `${skull_emote} **${skullCount}**`, embeds: [getEmbed(skullCount, reaction)]});
+        } else {
+            console.log("Skullpost not valid");
+        }
         return;
     }
 
