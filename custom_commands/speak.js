512d4c6a64bbe3628982b5d581843ac5309df24d
devmarcid <marcidkazuya@gmail.com>
2023-03-07 20:57:13 +0000

    fix echo

--- custom_commands/speak.js
+++ custom_commands/speak.js
@@ -2,7 +2,11 @@ const permissions = require("./../commands/data/permissions.json");
 
 function makeEcho(client, message) {
     if (permissions['echo'].includes(message.author.id.toString())) {
-        message.channel.send(message.content.split(" ").shift().join(" "))
+		var echo = message.content.split(" ");
+		echo.shift();
+		echo = echo.join(" ");
+        message.channel.send(echo);
+		message.delete();
         return;
     } else {
         message.reply("the allegations aren't true!!!");
