19a7adc0bdcc926350f46f56134c62b0097aad7f
devmarcid <marcidkazuya@gmail.com>
2023-03-07 20:52:22 +0000

    fixed ,speak

--- custom_commands/speak.js
+++ custom_commands/speak.js
@@ -2,7 +2,7 @@ const permissions = require("./../commands/data/permissions.json");
 
 function makeEcho(client, message) {
     if (permissions['echo'].includes(message.author.id.toString())) {
-        message.channel.send(message.content.split(" ").splice().join(" "))
+        message.channel.send(message.content.split(" ").splice(0, 1).join(" "))
         return;
     } else {
         message.reply("the allegations aren't true!!!");
