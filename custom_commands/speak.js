c6f092e070e5637f6061e122b64c03a98b0ff1dd
devmarcid <marcidkazuya@gmail.com>
2023-03-14 23:07:24 +0000

    Added try/catch to echo command

--- custom_commands/speak.js
+++ custom_commands/speak.js
@@ -2,12 +2,18 @@ const permissions = require("./data/permissions.json");
 
 function makeEcho(client, message) {
     if (permissions.includes(message.author.id.toString())) {
-		var echo = message.content.split(" ");
-		echo.shift();
-		echo = echo.join(" ");
-        message.channel.send(echo);
-		message.delete();
-        return;
+		try {
+			var echo = message.content.split(" ");
+			echo.shift();
+			echo = echo.join(" ");
+			message.channel.send(echo);
+			message.delete();
+			return;
+		} catch (error) {
+			console.log(error);
+			message.reply("stfu");
+			return;
+		}
     } else {
         message.reply("the allegations aren't true!!!");
         return;
