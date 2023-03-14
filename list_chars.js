310feddf6c45d4ead998819400a7563458e6e356
devmarcid <marcidkazuya@gmail.com>
2023-03-14 19:43:32 +0000

    test

--- list_chars.js
+++ list_chars.js
@@ -1,4 +1,4 @@
 const characters = require("./custom_commands/data/characters.json");
-//putting something here to commit
+//.putting something here to commit
 chars = characters.join("\n");
 console.log(chars);
